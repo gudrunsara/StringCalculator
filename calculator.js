a9455b494c575d6ca8b634496645afa5d7800046
Guðrún Sara <gudrun.sara@hotmail.com>
2018-10-13 20:36:34 +0000

    Add function implemented to return the sum of two numbers and a third test added in the test file

--- calculator.js
+++ calculator.js
@@ -2,8 +2,16 @@ function add(number){
   if(number == "")
   return 0;
   
-  // make number an int instead of a string:
-  return parseInt(number); 
+  // If there is a seperator in the number string:
+  if(number.includes(","))
+  {
+    var numberArray = number.split(",")
+    // return the sum of the two numbers:
+    return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+  }
+  else
+    // parseInt function casts from string to Int
+    return parseInt(number); 
 }
 
 module.exports = add;
