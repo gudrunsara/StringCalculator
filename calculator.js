80237207a9049e5d4bc843e82a0a230d5948f604
Guðrún Sara <gudrun.sara@hotmail.com>
2018-10-13 20:39:33 +0000

    Changing name of parameter number to numbers

--- calculator.js
+++ calculator.js
@@ -1,17 +1,17 @@
-function add(number){
-  if(number == "")
+function add(numbers){
+  if(numbers == "")
   return 0;
   
   // If there is a seperator in the number string:
-  if(number.includes(","))
+  if(numbers.includes(","))
   {
-    var numberArray = number.split(",")
+    var numberArray = numbers.split(",")
     // return the sum of the two numbers:
     return parseInt(numberArray[0]) + parseInt(numberArray[1]);
   }
   else
     // parseInt function casts from string to Int
-    return parseInt(number); 
+    return parseInt(numbers); 
 }
 
 module.exports = add;
--- calculator.test.js
+++ calculator.test.js
@@ -1,13 +1,18 @@
 const add = require("./calculator");
-
+// 1. test 
 it("should return zero on an empty string", () => {
     expect(add("")).toBe(0);
 });
 
+
+// 2. test
 it("should return number when only one number is in the string", () => {
   expect(add("5")).toBe(5);
 });
 
+
+// 3. test
 it("should return the sum of two given numbers", () => {
   expect(add("1,2")).toBe(3);
 });
+
