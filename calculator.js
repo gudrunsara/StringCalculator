ace8a4f5dbff1e54ecceeb71c23b435a271491be
Guðrún Sara <gudrun.sara@hotmail.com>
2018-10-13 20:47:17 +0000

    Add function implemented to calculate sum of multiple nr and fourth test implemented

--- calculator.js
+++ calculator.js
@@ -5,9 +5,15 @@ function add(numbers){
   // If there is a seperator in the number string:
   if(numbers.includes(","))
   {
-    var numberArray = numbers.split(",")
+    var numberArray = numbers.split(",");
+    var total = 0;
+    for(var i=0 ; i < numberArray.length ; i++)
+    {
+      total += parseInt(numberArray[i]);
+    }
+    return total;
     // return the sum of the two numbers:
-    return parseInt(numberArray[0]) + parseInt(numberArray[1]);
+    //return parseInt(numberArray[0]) + parseInt(numberArray[1]);
   }
   else
     // parseInt function casts from string to Int
--- calculator.test.js
+++ calculator.test.js
@@ -16,3 +16,9 @@ it("should return the sum of two given numbers", () => {
   expect(add("1,2")).toBe(3);
 });
 
+
+// 4. test
+it("should summarize multiple numbers", () => {
+  expect(add("1,2,3")).toBe(6);
+});
+
